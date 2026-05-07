6c374c2f4e42f6228d7a7f96a8270e13355d0380
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2026-05-07 20:25:51 +0300

    Create AppDbContext.cs

---
+++ backend/SuperChess.Api/Data/AppDbContext.cs
@@ -0,0 +1,58 @@
+using Microsoft.EntityFrameworkCore;
+using SuperChess.Api.Models;
+
+namespace SuperChess.Api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Player> Players => Set<Player>();
+    public DbSet<ChessGame> Games => Set<ChessGame>();
+    public DbSet<Move> Moves => Set<Move>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Player>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+            entity.Property(x => x.DisplayName).HasMaxLength(80).IsRequired();
+            entity.Property(x => x.SessionToken).HasMaxLength(200).IsRequired();
+        });
+
+        modelBuilder.Entity<ChessGame>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+            entity.Property(x => x.Status).HasMaxLength(30).IsRequired();
+            entity.Property(x => x.CurrentFen).HasMaxLength(200).IsRequired();
+            entity.Property(x => x.WhoseTurn).HasMaxLength(10).IsRequired();
+
+            entity.HasOne(x => x.WhitePlayer)
+                .WithMany()
+                .HasForeignKey(x => x.WhitePlayerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            entity.HasOne(x => x.BlackPlayer)
+                .WithMany()
+                .HasForeignKey(x => x.BlackPlayerId)
+                .OnDelete(DeleteBehavior.Restrict);
+        });
+
+        modelBuilder.Entity<Move>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+            entity.Property(x => x.Uci).HasMaxLength(20).IsRequired();
+            entity.Property(x => x.San).HasMaxLength(20);
+            entity.Property(x => x.PlayedByColor).HasMaxLength(10).IsRequired();
+
+            entity.HasOne(x => x.Game)
+                .WithMany(x => x.Moves)
+                .HasForeignKey(x => x.GameId)
+                .OnDelete(DeleteBehavior.Cascade);
+        });
+    }
+}

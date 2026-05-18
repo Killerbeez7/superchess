6a11c6cb8796a30c13349843d94d9f23800b8012
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2026-05-18 17:01:45 +0300

    chore(DB): Migrate PrefferedSetupOptions changes

---
+++ backend/SuperChess.Api/Migrations/20260518140103_RememberPreferredSetupOptions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SuperChess.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RememberPreferredSetupOptions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
